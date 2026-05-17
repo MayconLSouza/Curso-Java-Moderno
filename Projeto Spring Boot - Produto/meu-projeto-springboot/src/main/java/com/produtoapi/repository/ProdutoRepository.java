2ee575bfdff435da43ce87f299137969dcf2fb7d
Maycon <mayconl.martins@outlook.com>
2026-05-17 19:55:38 -0300

    Consultas avançadas: preço

--- Projeto Spring Boot - Produto/meu-projeto-springboot/src/main/java/com/produtoapi/repository/ProdutoRepository.java
+++ Projeto Spring Boot - Produto/meu-projeto-springboot/src/main/java/com/produtoapi/repository/ProdutoRepository.java
@@ -3,13 +3,30 @@ package com.produtoapi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.produtoapi.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	// Busca por nome
 	List<Produto> findByNome(String nome);
+
 	List<Produto> findByNomeContaining(String nome);
+
 	List<Produto> findByNomeAndStatus(String nome, String status);
+
 	List<Produto> findByNomeStartingWith(String prefix);
+
 	List<Produto> findByNomeEndingWith(String suffix);
+
+	// Busca por preço
+	List<Produto> findByPreco(Double preco);
+
+	List<Produto> findByPrecoGreaterThan(Double preco);
+
+	List<Produto> findByPrecoLessThan(Double preco);
+
+	@Query("SELECT SUM(p.preco) FROM Produto p")
+	Double findTotalPreco();
 }
